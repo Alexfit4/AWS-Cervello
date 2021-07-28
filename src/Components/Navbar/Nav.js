593b3cd01c38315a5a0fe0e71dba091c8687001b
Alex Ashtiany <alex.ashtiany@icloud.com>
2021-07-28 19:01:14 -0400

    update

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -33,7 +33,7 @@ function Navbars() {
 
 
   return (
-    <ProvideAuth>
+    
       <div className="main">
         <div className="row">
           <div className="col-md-12">
@@ -55,10 +55,6 @@ function Navbars() {
              
               <Switch>
                 <Route exact path="/" component={Main} />
-                <PrivateRoute path="/Internal" >
-                  <InternalHome />
-                </PrivateRoute>
-
                 {NavRoutes.map((x) => {
                   return <Route path={x.path} component={x.component} />;
                 })}
@@ -112,7 +108,7 @@ function Navbars() {
           or modification without Cervello's express consent in writing.
         </footer>
       </div>
-    </ProvideAuth>
+    
   );
 
 }
@@ -120,143 +116,5 @@ function Navbars() {
 
 
 
-
-const fakeAuth = {
-
-  isAuthenticated: false,
-  signin(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-
-/** For more details on
- * `authContext`, `ProvideAuth`, `useAuth` and `useProvideAuth`
- * refer to: https://usehooks.com/useAuth/
- */
-const authContext = createContext();
-
-function ProvideAuth({ children }) {
-  const auth = useProvideAuth();
-  return (
-    <authContext.Provider value={auth}>
-      {children}
-    </authContext.Provider>
-  );
-}
-
-function useAuth() {
-  return useContext(authContext);
-}
-
-function useProvideAuth() {
-  const [user, setUser] = useState("user");
-
-  const ionViewCanEnter = async () => {
-    return await Auth.currentAuthenticatedUser()
-      .then(() => setUser("user"))
-      .catch((err) => console.log(err));
-  }
-
-  ionViewCanEnter()
-
-
-
-
-  const signin = cb => {
-    return fakeAuth.signin(() => {
-      setUser("user");
-      cb();
-    });
-  };
-
-  const signout = cb => {
-    return fakeAuth.signout(() => {
-      setUser(null);
-      cb();
-    });
-  };
-
-  return {
-    user,
-    signin,
-    signout
-  };
-}
-
-function AuthButton() {
-  let history = useHistory();
-  let auth = useAuth();
-
-  return auth.user ? (
-    <p>
-      Welcome!{" "}
-      <button
-        onClick={() => {
-          auth.signout(() => history.push("/"));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  );
-}
-
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({ children, ...rest }) {
-  let auth = useAuth();
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        auth.user ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/Login",
-              state: { from: location }
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
-function PublicPage() {
-  return <h3>Public</h3>;
-}
-
-function ProtectedPage() {
-  return <h3>Protected</h3>;
-}
-
-function LoginPage() {
-  let history = useHistory();
-  let location = useLocation();
-  let auth = useAuth();
-
-  let { from } = location.state || { from: { pathname: "/" } };
-  let login = () => {
-    auth.signin(() => {
-      history.replace(from);
-    });
-  };
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
-    </div>
-  );
-}
 
 export default Navbars;
